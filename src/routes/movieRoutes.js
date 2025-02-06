b15daee8b69df558db750ebab00892dc69b46cb0
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-02-05 23:17:43 -0300

    feat: add DELETE route to delete movie

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -5,3 +5,4 @@ const movieController = require("../controllers/movieController");
 router.get("/movies", movieController.getAllMovies);
 router.post("/movies", movieController.addMovie);
 router.put("/movies",movieController.updateMovie);
+router.delete("/movies/:id", movieController.deleteMovie);
