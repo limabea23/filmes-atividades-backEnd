dbae573881ffb21e520200fab9df012262f82bc3
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-02-05 23:11:44 -0300

    feat: add GET route to list movies

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -1,3 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const movieController = require("../controllers/movieController");
+
+router.get("/movies", movieController.getAllMovies);
