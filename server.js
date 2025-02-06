7ff98d19f9a2201574a593b43d0447a5dba78be7
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-02-05 23:29:29 -0300

    feat: create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const movieRoutes = require("./src/routes/movieRoutes");
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", movieRoutes);
+
+app.get("/", (req, res) => {
+    res.send("HARRY STYLES IS THE BEST 💗🌟🤠💋");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
